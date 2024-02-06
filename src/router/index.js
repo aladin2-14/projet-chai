22a72d7b65981dfe65fcd0ee495076e1c1ff49b1
aladin2-14 <robertbellmugisha@gmail.com>
2024-02-06 17:59:04 +0200

    premiere

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from "../views/PageHome.vue"
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+  
+    {
+      path:"/",
+      name:"pagehome",
+      component:PageHome
+    }
+  ]
+})
+
+export default router
---
+++ src/views/PageHome.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="un">
+ <header>
+    <div class="headers-un">
+        <img src="../assets/logo-white.png" alt="">
+        <ul>
+            <li>About</li>
+            <li>Features</li>
+            <li>Page</li>
+            <li>Pricing</li>
+            <li>FAQs</li>
+
+        </ul>
+        <div class="headers-deux">
+            <h3>Sign in</h3>
+            <div class="un-un">
+                <button>Sign up</button>
+            </div>
+        </div>
+    </div>
+       
+       
+    </header>
+    <div class="un-un"></div>
+    </div>
+   
+</template>
+
+<script>
+export default {
+   
+}
+</script>
+
+<style>
+.un{
+    background-image:url(../assets/img/hero-1.jpg) ;
+    height: 70vw;
+    position: relative;
+    
+}
+.headers{
+    position: fixed;
+    z-index: 1000;
+    display: flex;
+}
+.headers-un{
+    display: flex;
+    width: 100% ;
+    gap: 10vw;
+    position: relative;
+}
+.headers-un ul {
+    display: flex;
+    list-style-type: none;
+    gap: 2vw;
+    margin-top: 3vw;
+    position: relative;
+}
+.headers-un li{
+    color: aliceblue;
+}
+.headers-un img{
+    left: 0;
+    width: 10vw;
+    margin-top: 3vw;
+    position: relative;
+}
+.headers-deux{
+    display: flex;
+    color: aliceblue;
+    gap: 2vw;
+    margin-top: 3vw;
+    position: relative;
+}
+.un-un button{
+    width: 7vw;
+    height: 3vw;
+    border-radius: 9px;
+}
+</style>
