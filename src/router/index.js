2fba2e6200cf94c02fb348ab68520d0a2d1d80c0
DonJosue3 <ishakaryayodonjosue@gmail.com>
2024-02-08 17:41:28 +0200

    quatre

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from "../views/PageHome.vue"
-import text from "../views/text.vue"
-import picture from "../views/picture.vue"
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -10,16 +9,6 @@ const router = createRouter({
       path:"/",
       name:"pagehome",
       component:PageHome
-    },
-    {
-      path:"/text",
-      name:"pagetext",
-      component:text
-    },
-    {
-      path:"/picture",
-      name:"pagepicture",
-      component:picture
     }
   ]
 })
